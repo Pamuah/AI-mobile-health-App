f518b20fbdce5491528f74f33cab26e6a5038e42
Patrick Amuah <patrickamuah38@gmail.com>
2024-05-21 07:31:26 +0000

    doctor screen updated

--- lib/Screens/Doctor.dart
+++ lib/Screens/Doctor.dart
@@ -9,16 +9,19 @@ class DoctorScreen extends StatefulWidget {
 }
 
 class _DoctorScreenState extends State<DoctorScreen> {
+  TextEditingController userTextController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    final color = Theme.of(context).colorScheme;
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          "Appointment",
+          "Consultation",
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
-        toolbarHeight: 80,
+        toolbarHeight: 70,
         leading: IconButton(
           onPressed: () {
             Navigator.of(context).pop();
@@ -26,22 +29,62 @@ class _DoctorScreenState extends State<DoctorScreen> {
           icon: const Icon(Icons.arrow_back_ios_new_rounded),
         ),
       ),
-      body: const SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              "Screen Currently Under Construction...",
-              style: TextStyle(
-                  fontSize: 22, fontWeight: FontWeight.bold, color: Colors.red),
-            ),
-            Icon(
-              Icons.streetview_outlined,
-              size: 150,
-              color: Colors.red,
-            ),
-          ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Spacer(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 50,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: color.primary),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: TextField(
+                        controller: userTextController,
+                        decoration: InputDecoration(
+                          hintText: 'Type here...',
+                          hintStyle: TextStyle(
+                            color: color.tertiary,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 16,
+                          ),
+                          border: InputBorder.none,
+                          contentPadding: EdgeInsets.symmetric(horizontal: 16),
+                        ),
+                      ),
+                    ),
+                  ),
+                 
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: color.primary,
+                      ),
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.send,
+                          size: 26,
+                          color: color.onPrimary,
+                        ),
+                      ),
+                    ),
+                ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
